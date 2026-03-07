66263b0e8d0b0c5df55fb5688161f4ff8c9c3765
Sachin Rajput <sachinrajput988724@gmail.com>
2026-03-08 00:05:23 +0530

    Fix NewsApiController import path

--- backend/routes/NewsApiRoutes/newsApiRoute.js
+++ backend/routes/NewsApiRoutes/newsApiRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {fetchNewsApi,fetchNewsApiSearch} from "../../controllers/NewsApiController/newsApiController.js";
+import {fetchNewsApi,fetchNewsApiSearch} from "../../controllers/NewsApiController/NewsApiController.js";
 const newsApiRouter = express.Router();
 
 newsApiRouter.get("/newsapinews", fetchNewsApi);
